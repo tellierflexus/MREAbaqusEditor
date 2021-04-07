27070cff55eb106ee0a03387a1b737c021468c93
frequencebanane <presidence@frequencebanane.ch>
2021-04-07 23:33:38 +0200

    Add material and step modification, functionnal for the moment but no dummy mesh

--- abaqus.py
+++ abaqus.py
@@ -17,7 +17,12 @@ parser.add_argument('--Gshear', default='400e3', action='store', help='X compone
 parser.add_argument('--Kbulk', default='400e5', action='store', help='Y component of the unit vector magnetic field')
 
 args = parser.parse_args()
-
+match = re.search(r'(.+)[.]inp$', args.file)
+if match:
+	filename = match.expand(r'\1') 
+else :
+	print("Please enter a valid filename with extension (only inp)")
+	exit()
 try:
 	with open(args.file, 'r') as inp_file:
 		content = inp_file.read()
@@ -79,6 +84,50 @@ element = """*******************************************************************
 *Element, type=U3"""
 
 dummy_mesh = "*Elset, elset=$elset$_dummy_mesh, generate\n10001, $LastDummyElement$, 1\n"
+
+materials = """******************************************************************
+*uel property,elset=$INSTANCE$.$ELSET$
+<Gshear>,<Kbulk>,<Mx>,<My>,<Mz>,<nInt>
+******************************************************************
+**
+******************************************************************
+*Material, name=Material-0
+*Conductivity
+1.,
+*Elastic
+ 1e-20,0. 
+******************************************************************\n"""
+
+initials_conditions = """************************************************************************
+**			INITIAL CONDITIONS
+*Initial conditions, type=field, variable=1
+$INSTANCE$.$ELSET$,<magFieldVecX>
+*Initial conditions, type=field, variable=2
+$INSTANCE$.$ELSET$,<magFieldVecY>
+*Initial conditions, type=field, variable=3
+$INSTANCE$.$ELSET$,<magFieldVecZ>
+*Initial conditions, type=field, variable=4
+$INSTANCE$.$ELSET$,0.0
+************************************************************************
+**
+************************************************************************
+**			AMPLITUDE DEFINITION
+*Amplitude, name=magFieldMagAmp
+0.0,0.0,1.,<magFieldMag>
+************************************************************************\n"""
+
+applied_field = """************************************************************************
+**			APPLIED MAGNETIC FIELD
+**
+*Field, variable=1
+$INSTANCE$.$ELSET$,<magFieldVecX>
+*Field, variable=2
+$INSTANCE$.$ELSET$,<magFieldVecY>
+*Field, variable=3
+$INSTANCE$.$ELSET$,<magFieldVecZ>
+*Field, variable=4, amplitude=magFieldMagAmp
+$INSTANCE$.$ELSET$,1.0
+************************************************************************\n"""
 """
 
 
@@ -117,6 +166,15 @@ else:
 
 dummy_mesh = re.sub(r'(?:[$]LastDummyElement[$])', str(10001 + nbrOfElem), dummy_mesh)
 dummy_mesh = re.sub(r'(?:[$]elset[$])', elset, dummy_mesh)
+
+initials_conditions = re.sub( r'(?:[$]INSTANCE[$])',  Instance, initials_conditions)
+initials_conditions = re.sub( r'(?:[$]ELSET[$])',  elset, initials_conditions)
+
+applied_field = re.sub( r'(?:[$]INSTANCE[$])',  Instance, applied_field)
+applied_field = re.sub( r'(?:[$]ELSET[$])',  elset, applied_field)
+
+materials = re.sub( r'(?:[$]INSTANCE[$])',  Instance, materials)
+materials = re.sub( r'(?:[$]ELSET[$])',  elset, materials)
 """
 
 
@@ -128,9 +186,30 @@ dummy_mesh = re.sub(r'(?:[$]elset[$])', elset, dummy_mesh)
 
 content = re.sub(r'((?:[*]{2}\s[*]{2}\sPARTS\n))', parameters + r'\1', content)
 content = re.sub(r'(?:[*]Element,\stype=C3D8RH)',element, content)
-if (~args.dummy):
-	dummy_mesh=''
-print(args.dummy)
-content = re.sub(r'(?:[*]{2}\sSection:.+\s.+\s,\s)', dummy_mesh, content)
+#if (~args.dummy):
+#	dummy_mesh=''
+#print(args.dummy)
+content = re.sub(r'(?:[*]{2}\sSection:.+\s.+\s,\s)', dummy_mesh, content) # Add the dummy mesh elset
 
-print(content)
+match = re.search(r'(?:[*]Surface.+name=(.+)\s.+\s)', content) #Detection of a surface
+if match:
+	Surface = match.expand(r'\1')
+	content = re.sub(r'(?:[*]Surface.+name=(.+)\s.+\s)', '', content) #Deletion of surface definition
+	content = re.sub(r'(?:[*]Dsload(.+\s){2})', '', content) #Deletion of Distributed surface load definition (temporary)
+
+content = re.sub(r'((?:[*]{2}\s\n[*]{2}\sSTEP:(\s.+){5}\s))',initials_conditions + r'\1' + applied_field, content ) #Add initial conditions then applied fied to step
+content = re.sub(r'((?:[*]{2}\s\n[*]{2}\sMATERIALS\s.+\s))',r'\1' + materials, content ) # Add definitions of new materials to the file
+
+try:
+	with open(filename + '_modified.inp', 'w') as inp_file:
+		inp_file.write(content)
+except FileNotFoundError:
+	print("The file specified cannot be found")
+	exit()
+except:
+	print("Unexpected error:", sys.exc_info()[0])
+	raise
+
+
+
+#print(content)
